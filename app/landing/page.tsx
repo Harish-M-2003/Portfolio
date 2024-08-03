2bed745046880b621fec2560247229a57892ee89
harishm <harishm762003@gmail.com>
2024-08-03 21:06:04 +0530

    fix : fixed home section ui

--- app/landing/page.tsx
+++ app/landing/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Alert from "./Components/Alert";
-import { BsArrowDownCircleFill } from "react-icons/bs";
+import { BsArrowDownCircleFill, BsGithub, BsLinkedin } from "react-icons/bs";
 import Link from "next/link";
 import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
@@ -13,72 +13,68 @@ extend({ Model });
 
 export default function HomeSection() {
   return (
-    <div className="h-screen  w-screen flex  max-sm:relative">
-      <div className="w-full flex flex-col relative">
-        <Image
-          draggable={false}
-          alt="img"
-          src={require("../india.jpg")}
-          style={{ zIndex: 1 }}
-          width={700}
-          className="opacity-[0.1] absolute max-md:hidden blur-sm"
-        />
-        <div className="h-[70%] flex justify-center md:items-end flex-col">
-          <div className="h-[10rem] flex justify-center items-center" />
-          {/* <div className="w-full sm:hidden h-screen z-10 ">
-            <Suspense fallback={<p>Loading</p>}>
-              <Canvas className="h-screen">
-                <OrbitControls maxDistance={7} />
-                <Model />
-              </Canvas>
-            </Suspense>
-          </div> */}
-          <h1
-            className="md:text-4xl text-3xl text-white text-center "
-            style={{ fontFamily: "Orbitron" }}
-          >
-            Hi! I am
-            <span className="text-blue-300 md:text-6xl"> #Harish M </span>
-            <br />
-            Student At{" "}
-            <span className="text-blue-300 md:text-6xl md:hidden">#CIT</span>
-            <span className="text-blue-300 md:text-6xl max-md:hidden">
-              #Chennai{" "}
-            </span>
-            <span className="max-md:hidden">institute of technology</span>
-          </h1>
-
-          {/* <div className="flex justify-end text-white pt-10 ">
-            <span className="max-sm:text-lg" style={{ fontFamily: "Orbitron" }}>
-              - Edward Teller
-            </span>
-          </div> */}
-        </div>
-        <div className="h-[30%] flex items-center sm:items-start z-10 max-md:pb-20 md:pt-20">
-          <div className="flex justify-between sm:px-10 px-2  w-full gap-2">
-            <Alert action={"LinkedIn"} />
-            <div className="max-sm:hidden">
-              <Alert action={"Github"} />
-            </div>
-            <Link
-              target="_blank"
-              //  download
-              href={"#"}
-              //  href={"/Harish-M.pdf"}
-              // passHref
-              className="border text-white flex items-center gap-5 text-black font-bold border-1 py-4 px-[2rem] hover:bg-transparent md:px-20 rounded-lg"
+    <div className="h-screen  w-screen grid grid-cols-2  max-sm:relative">
+      <Image
+        draggable={false}
+        alt="img"
+        src={require("../india.jpg")}
+        style={{ zIndex: 1 }}
+        width={700}
+        className="opacity-[0.1] absolute max-md:hidden blur-sm"
+      />
+      <div className="">
+        <div className="h-screen grid grid-rows-1 pl-5">
+          <div className="flex justify-center flex-col gap-5 items-center">
+            <h1
+              className="md:text-5xl text-3xl text-white px-10 text-center"
+              style={{ fontFamily: "Orbitron" }}
             >
-              <BsArrowDownCircleFill className="max-sm:text-3xl" />
-              <button 
-          style={{fontFamily : "Orbitron"}}>Resume</button>
-            </Link>
+              Hi! I am
+              <span className="text-blue-300 md:text-5xl font-bold"> # Harish M </span>
+              <br />
+            </h1>
+            <div className="text-center">
+              <span className="text-xl text-white font-[100]">LEARN SOMETHING ABOUT EVERYTHING AND EVERYTHING ABOUT SOMETHING</span>
+            </div>
+            <div className="flex justify-between pt-19 p-2 z-10 w-full gap-2">
+              <Link
+                target="_blank"
+                href={"#"}
+                className="border border-gray-600 hover:border-gray-300 text-white flex items-center gap-5 text-black font-bold border-1 p-4 px-8 pr-14 bg-transparent  rounded-lg"
+              >
+                <BsLinkedin className="max-sm:text-3xl" />
+                <button
+                  style={{ fontFamily: "Orbitron" }}>LINKEDIN</button>
+              </Link>
+              <Link
+                target="_blank"
+                href={"#"}
+                className="border border-gray-600 hover:border-gray-300 text-white flex items-center gap-5 text-black font-bold border-1 p-4 px-8 pr-14 bg-transparent rounded-lg"
+              >
+                <BsGithub className="max-sm:text-3xl" />
+                <button
+                  style={{ fontFamily: "Orbitron" }}>Github</button>
+              </Link>
+              <Link
+                target="_blank"
+                //  download
+                href={"#"}
+                //  href={"/Harish-M.pdf"}
+                // passHref
+                className="border border-gray-600 hover:border-gray-300   text-white flex items-center gap-5 text-black font-bold border-1 p-4 px-8 pr-14 bg-transparent  rounded-lg"
+              >
+                <BsArrowDownCircleFill className="max-sm:text-3xl" />
+                <button
+                  style={{ fontFamily: "Orbitron" }}>Resume</button>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
-      <div className="w-full max-sm:hidden">
+      <div className="max-sm:hidden">
         <Suspense fallback={<p>Loading</p>}>
           <Canvas className="sm:h-[100vh]">
-            <OrbitControls maxDistance={7} />
+            <OrbitControls maxDistance={7} enableZoom = {false}/>
             <Model />
           </Canvas>
         </Suspense>
--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className='scroll-smooth'>
       {/* <Image
           alt="img"
           src={require("../app/india.jpg")}
